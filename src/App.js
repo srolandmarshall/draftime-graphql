828d46a989b3b0af75240f32f4e682b49557deef
srolandmarshall <samuel.r.marshall@gmail.com>
2020-08-19 00:03:38 -0400

    commiting to rebuild

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Hello from V2</h1>
+				<h1>DRAFTime</h1>
 			</header>
 		</div>
 	);
