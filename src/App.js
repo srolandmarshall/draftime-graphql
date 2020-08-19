d840145040503ddf4d7b1b110e64be39fae45cd2
srolandmarshall <samuel.r.marshall@gmail.com>
2020-08-19 01:31:02 -0400

    final try before giving up

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Please build?</h1>
+				<h1>Last Attempt Before Bed</h1>
 			</header>
 		</div>
 	);
